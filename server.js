3f23a3452cd360ed00dc873e29ee40ef10c80592
Ashley Waldner <ashleywaldner22@gmail.com>
2020-07-04 09:04:25 -0500

    uncomment line

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.set("view engine", "handlebars");
 // mongoose.connect("mongodb://heroku_jmv816f9:5j1nd4taq42hi29bfm5hobeujd@ds133192.mlab.com:33192/heroku_jmv816f9");
 // // mongoose.connect("mongodb://localhost/mongoscraper");
 
-// var db = mongoose.connection;
+var db = mongoose.connection;
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI)
